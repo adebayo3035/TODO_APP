91635f3836ec07466d690303908b9a97308a189a
abdul-rahmon <abdulrahmonadebayo@gmail.com>
2023-03-25 10:48:46 +0100

    Added fiunctionality to save Data to localStorage and retrieve Data from localStorage

--- script.js
+++ script.js
@@ -14,13 +14,30 @@ const addTask = () =>{
         li.appendChild(span)
     }
     inputBox.value = " "
+    //call SaveData function whenever a new task has been added.
+    saveData();
 }
-
+// Add click event to ListContainer so than tasks can be marked as Completed and deleted
 listContainer.addEventListener("click", (e) => {
     if(e.target.tagName == "LI"){
         e.target.classList.toggle("checked");
+        //Save Data when a task is marked as Completed
+        saveData();
     }
     else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove()
+        //saveData when a Task has been Deleted
+        saveData();
     }
 }, false);
+
+//function to allow tasks to save on local Storage when Browser is refreshed or closed
+
+const saveData = ()=>{
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+// function to display Data when the app is visited again
+const showTask = () =>{
+    listContainer.innerHTML = localStorage.getItem("data")
+}
